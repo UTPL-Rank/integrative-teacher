c4bd81bab5e61b7c2eb068ecf020cd4131059385
unknown <pablopicon@gmail.com>
2021-12-01 18:16:49 -0500

    home-page,documents,dashboard-home

--- integrative-teacher/src/app/shared/shared.module.ts
+++ integrative-teacher/src/app/shared/shared.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IconsModule } from './icons/icons.module';
-
+//import { NgxDocViewerModule } from 'ngx-doc-viewer';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     IconsModule,
+    //NgxDocViewerModule
   ],
   exports: [
     IconsModule,
+    //NgxDocViewerModule
   ]
 })
 export class SharedModule { }
