03ef417782f0fc65ce90947dba37ececfa4d08da
Danilo Mendoza <odmendoza@utpl.edu.ec>
2022-01-28 01:47:13 -0500

    Upload files, swal messages

--- integrative-teacher/src/app/upload/pages/upload-faculties/upload-faculties.component.ts
+++ integrative-teacher/src/app/upload/pages/upload-faculties/upload-faculties.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { DocumentReference } from '@angular/fire/firestore';
 import { Faculty } from '../../../models/faculty';
-import { Degree } from '../../../models/degree';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-upload-faculties',
@@ -18,7 +17,12 @@ export class UploadFacultiesComponent {
 
   async save(): Promise<void> {
     if (!this.data) {
-      alert('Primero lee el archivo de los carreras académicas.');
+      await Swal.fire(
+        {
+          title: 'No se puede realizar esta acción. Por favor, cargue el archivo de las facultades académicas.',
+          icon: 'error'
+        }
+      );
       return;
     }
 
@@ -34,12 +38,11 @@ export class UploadFacultiesComponent {
       });
 
       await batch.commit();
-      alert('Todos las carreras han sido guardados.');
-      // this.router.navigateByUrl('/');
+      await Swal.fire({title: 'Las facultades han sido guardadas exitosamente.', icon: 'success'});
+      this.data = null;
     } catch (error) {
       this.isSaving = false;
-      console.log(error);
-      alert('Ocurrió un error al guardar las carreras, vuelve a intentarlo.');
+      await Swal.fire({title: 'Ocurrió un error al guardar las facultades, vuelve a intentarlo.', icon: 'error'});
     }
   }
   async transformer(rawData: string[]): Promise<Faculty> {
